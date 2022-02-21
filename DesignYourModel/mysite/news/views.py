1c987c6297e00c22231e1fbae8ecdc0a950e4d79
otfreetime <omaraltaezi@gmail.com>
2022-02-21 13:07:02 +0300

    Update

--- DesignYourModel/mysite/news/views.py
+++ DesignYourModel/mysite/news/views.py
@@ -1,9 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
 
 from .models import Article
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
 
 def year_archive(request, year):
     a_list = Article.objects.filter(pub_date__year=year)
